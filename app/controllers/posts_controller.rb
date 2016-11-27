54128d574ea4b7b5baea7f35d1953b11ee424b8c
Naoto Taniguchi <ntaniguchi94@gmail.com>
2016-11-27 08:59:31 +0000

    リファクタリング

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
+ before_action :set_post, only: [ :show, :edit, :update, :destroy]
   def index
+    @posts = Post.all
+    @new_posts = Post.all
   end
 
   def show
@@ -16,12 +19,7 @@ class PostsController < ApplicationController
     redirect_to "/posts/#{@post.id}" 
   end
 
-  private
 
-  def post_params 
-    params.require(:post).permit(:title, :body, :category)
-  end
-end
 
   def edit
     @post = Post.find(params[:id])
@@ -35,3 +33,13 @@ end
 
   def destroy
   end
+  
+  private
+
+  def post_params 
+    params.require(:post).permit(:title, :body, :category)
+  end
+  def set_post
+    @post = Post.find(params[:id])
+  end
+end
